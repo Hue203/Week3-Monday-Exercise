34505c4cd74a7d8f94a6c3be8c5c7b82d65636bd
Hue203 <huenguyenmt.vn@gmail.com>
2021-07-05 16:09:51 +0700

    description

--- script.js
+++ script.js
@@ -1,193 +1,195 @@
-// // const name = 'Parrot'
-// // const colors = ['Red', 'Green', 'Blue', 'Yellow']
+///2.Object Shorthand
+//2.1.Refactor to use object shorthand when defining the property & value pairs which describe bird.
+const name = "Parrot";
+const colors = ["Red", "Green", "Blue", "Yellow"];
 
-// // const bird = {
-// //   name: name,
-// //   colors: colors
-// // }
+const bird = {
+  name: name,
+  colors: colors,
+};
 
-// // console.log(bird)
+console.log(bird);
+//2.2.Refactor the code with object shorthand when defining the property value pairs which describe bird such that a property talons is true. Use object property shorthand.
+const name = "Parrot";
+const colors = ["Red", "Green", "Blue", "Yellow"];
+const talons = ["Prey", "Eagles", "Hawks", "Falcons"];
 
-// const name = "Parrot";
-// const colors = ["Red", "Green", "Blue", "Yellow"];
-// const talons = ["Prey", "Eagles", "Hawks", "Falcons"];
+const bird = {
+  name: name,
+  colors: colors,
+  talons: talons,
+};
 
-// const bird = {
-//   name: name,
-//   colors: colors,
-//   talons: talons,
-// };
+console.log(bird);
+//2.3. Define an object vietnam using the variables capital & continent. Use shorthand.
+const capital = "Hanoi";
+const continent = "Asia";
 
-// console.log(bird);
+const vietnam = {
+  capital,
+  continent,
+};
 
-// const capital = "Hanoi";
-// const continent = "Asia";
+console.log(vietnam);
+//2.4.Define an object car using the variables wheelsCount, doorsCount, & color as property shorthand.
+const wheelsCount = 4;
+const doorsCount = 4;
+const color = "black";
 
-// const vietnam = {
-//   capital,
-//   continent,
-// };
+const car = {
+  wheelsCount,
+  doorsCount,
+  color,
+};
 
-// console.log(vietnam);
+console.log(car);
 
-// const wheelsCount = 4;
-// const doorsCount = 4;
-// const color = "black";
+////3. Destructuring.///
 
-// const car = {
-//   wheelsCount,
-//   doorsCount,
-//   color,
-// };
+function getAverage(obj) {
+  const { x, y, z } = obj;
+  return Math.floor((x + y + z) / 3.0);
+}
 
-// console.log(car);
+console.log("getAverage", getAverage({ x: 3.6, y: 7.8, z: 4.3 }));
 
-// ////3. Destructuring.///
+function getAddress() {
+  let coderSchool = {
+    city: "HCM",
+    country: "Vietnam",
+    address: {
+      number: 12,
+      street: "Ton Dan",
+      district: "4",
+    },
+  };
+  const {
+    city,
+    country,
+    address: { street },
+  } = coderSchool;
+  return city === "HCMC" && country === "Vietnam" && street === "Ton Dan";
+}
 
-// function getAverage(obj) {
-//   const { x, y, z } = obj;
-//   return Math.floor((x + y + z) / 3.0);
+console.log(getAddress());
+
+function objectifyElements() {
+  const arr = [0, 1, 2, 4, 3, 5, 6, 7, 8];
+  const [first, , third, fourth] = arr;
+  return { first, third, fourth };
+}
+
+console.log(objectifyElements([0, 1, 2, 3, 4, 5, 6, 7, 8]).fourth === 4);
+
+///
+
+function getFoodItems() {
+  const food = [
+    ["carrots", "beans", "peas", "lettuce"],
+    ["apples", "mangos", "oranges"],
+    ["cookies", "cake", "pizza", "chocolate"],
+  ];
+  // const carrots = food[0][0];
+  // const cookies = food[2][0];
+  // const mangos = food[1][1];
+  const [[carrots], [, mangos], [cookies]] = food;
+  return { carrots, cookies, mangos };
+}
+
+console.log(getFoodItems().mangos === "mangos");
+
+/// Refactor so that the return is true //
+
+// function getHistoricPrices(index) {
+//   const bingo = index[0];
+//   return bingo;
 // }
 
-// console.log("getAverage", getAverage({ x: 3.6, y: 7.8, z: 4.3 }));
+// console.log(getHistoricPrices([true, false, true]));
 
-// function getAddress() {
-//   let coderSchool = {
-//     city: "HCM",
-//     country: "Vietnam",
-//     address: {
-//       number: 12,
-//       street: "Ton Dan",
-//       district: "4",
-//     },
-//   };
-//   const {
-//     city,
-//     country,
-//     address: { street },
-//   } = coderSchool;
-//   return city === "HCMC" && country === "Vietnam" && street === "Ton Dan";
+// function getHistoricPrices(index) {
+//   const bingo = index[0][0];
+//   return bingo;
 // }
 
-// console.log(getAddress());
+// console.log(
+//   getHistoricPrices([
+//     [true, false],
+//     [false, true],
+//   ])
+// );
 
-// function objectifyElements() {
-//   const arr = [0, 1, 2, 4, 3, 5, 6, 7, 8];
-//   const [first, , third, fourth] = arr;
-//   return { first, third, fourth };
+// function getHistoricPrices(index) {
+//   const bingo = index[0][0][0];
+//   return bingo;
 // }
 
-// console.log(objectifyElements([0, 1, 2, 3, 4, 5, 6, 7, 8]).fourth === 4);
+// console.log(
+//   getHistoricPrices([
+//     [
+//       [true, false],
+//       [true, false],
+//     ],
+//   ])
+// );
 
-// ///
+//// 4. Rest and Spread.///
 
-// function getFoodItems() {
-//   const food = [
-//     ["carrots", "beans", "peas", "lettuce"],
-//     ["apples", "mangos", "oranges"],
-//     ["cookies", "cake", "pizza", "chocolate"],
-//   ];
-//   // const carrots = food[0][0];
-//   // const cookies = food[2][0];
-//   // const mangos = food[1][1];
-//   const [[carrots], [, mangos], [cookies]] = food;
-//   return { carrots, cookies, mangos };
-// }
+function restParams(first, ...rest) {
+  return first === "first" && rest[0] === "second";
+}
 
-// console.log(getFoodItems().mangos === "mangos");
+console.log(restParams("first", "second"));
 
-// /// Refactor so that the return is true //
+function restParams2(first, ...rest) {
+  return first === "first" && rest[0] === "second" && rest[1] === "third";
+}
 
-// // function getHistoricPrices(index) {
-// //   const bingo = index[0];
-// //   return bingo;
-// // }
+console.log(restParams2("first", "second", "third"));
 
-// // console.log(getHistoricPrices([true, false, true]));
+function restParams3(first, ...rest) {
+  return (
+    first === "first" &&
+    rest[0] === "second" &&
+    rest[1] === "third" &&
+    rest[2] === undefined
+  );
+}
 
-// // function getHistoricPrices(index) {
-// //   const bingo = index[0][0];
-// //   return bingo;
-// // }
+console.log(restParams3("first", "second", "third")); // CHANGE ME
 
-// // console.log(
-// //   getHistoricPrices([
-// //     [true, false],
-// //     [false, true],
-// //   ])
-// // );
+function restParams4(...rest) {
+  return rest[2] === "first" && rest[0] === "second" && rest[1] === "third"; // CHANGE ME
+}
 
-// // function getHistoricPrices(index) {
-// //   const bingo = index[0][0][0];
-// //   return bingo;
-// // }
+console.log(restParams4("second", "third", "first"));
 
-// // console.log(
-// //   getHistoricPrices([
-// //     [
-// //       [true, false],
-// //       [true, false],
-// //     ],
-// //   ])
-// // );
+function ontoAnObject() {
+  const array = [1, 2, 3, 4, 5, 6];
+  const object = {};
+  [object.one, object.two, object.three, ...object.rest] = array;
 
-// //// 4. Rest and Spread.///
+  return object;
+}
 
-// function restParams(first, ...rest) {
-//   return first === "first" && rest[0] === "second";
-// }
+console.log(ontoAnObject());
 
-// console.log(restParams("first", "second"));
+function findTheMax() {
+  const arr1 = [1, 7, 2, 4];
+  const arr2 = [1, 9, 5, 8];
+  return Math.max(...arr1, ...arr2);
+}
+console.log(findTheMax());
 
-// function restParams2(first, ...rest) {
-//   return first === "first" && rest[0] === "second" && rest[1] === "third";
-// }
+function concatenateArrays() {
+  const arr1 = [0, 1, 2, 3];
+  const arr2 = [4, 5, 6];
+  const arr3 = [7, 8, 9];
+  const result = [...arr1, ...arr2, ...arr3];
+  return result;
+}
 
-// console.log(restParams2("first", "second", "third"));
-
-// function restParams3(first, ...rest) {
-//   return (
-//     first === "first" &&
-//     rest[0] === "second" &&
-//     rest[1] === "third" &&
-//     rest[2] === undefined
-//   );
-// }
-
-// console.log(restParams3("first", "second", "third")); // CHANGE ME
-
-// function restParams4(...rest) {
-//   return rest[2] === "first" && rest[0] === "second" && rest[1] === "third"; // CHANGE ME
-// }
-
-// console.log(restParams4("second", "third", "first"));
-
-// function ontoAnObject() {
-//   const array = [1, 2, 3, 4, 5, 6];
-//   const object = {};
-//   [object.one, object.two, object.three, ...object.rest] = array;
-
-//   return object;
-// }
-
-// console.log(ontoAnObject());
-
-// function findTheMax() {
-//   const arr1 = [1, 7, 2, 4];
-//   const arr2 = [1, 9, 5, 8];
-//   return Math.max(...arr1, ...arr2);
-// }
-// console.log(findTheMax());
-
-// function concatenateArrays() {
-//   const arr1 = [0, 1, 2, 3];
-//   const arr2 = [4, 5, 6];
-//   const arr3 = [7, 8, 9];
-//   const result = [...arr1, ...arr2, ...arr3];
-//   return result;
-// }
-
-// console.log(concatenateArrays());
+console.log(concatenateArrays());
 
 function mergeObjects() {
   // what does this return?
